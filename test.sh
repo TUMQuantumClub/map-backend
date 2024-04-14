9ba2a18b7a10e4125d2aa414e05c683a5b15b0ce
biosfood <leisenhau@gmail.com>
2024-04-14 16:23:11 +0200

    run add action to run prettier

--- test.sh
+++ test.sh
@@ -6,6 +6,8 @@ npm i
 
 npm run start &
 
+npx prettier . --check
+
 # wait for the server to start running
 while [[ "$(curl -s -o /dev/null -w '%{http_code}' 'localhost:4000')" == "000" ]]; do sleep 1; done
 
