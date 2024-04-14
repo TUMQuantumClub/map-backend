d6ae3e368f540ea5285bddda467e5d03dcef1240
biosfood <leisenhau@gmail.com>
2024-04-14 16:13:30 +0200

    add first test

---
+++ test.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+set -e
+
+npm i
+
+npm run start &
+
+# wait for the server to start running
+while [[ "$(curl -s -o /dev/null -w '%{http_code}' 'localhost:4000')" == "000" ]]; do sleep 1; done
+
+# start jest
+npm run run-test --coverage || true
+
+# kill the server again
+killall node
