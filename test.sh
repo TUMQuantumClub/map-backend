6791caf423ea62d6aa49490f4fa9179ee649bee8
biosfood <leisenhau@gmail.com>
2024-04-14 19:15:58 +0200

    move prettier check to the github action

--- test.sh
+++ test.sh
@@ -3,9 +3,6 @@
 nohup npm run start &
 serverPID=$!
 
-npx prettier . --check
-prettier_result=$?
-
 # wait for the server to start running
 while [[ "$(curl -s -o /dev/null -w '%{http_code}' 'localhost:4000')" == "000" ]]; do sleep 1; done
 
@@ -16,15 +13,7 @@ test_result=$?
 # kill the server again
 kill $serverPID
 
-if [[ "$prettier_result" != "0" ]]; then
-  echo "prettier failed! Run ´npm run format´ to automatically fix this"
-  if [[ "$test_result" != "0" ]]; then
-    echo "tests failed as well!"
-  fi
-  exit 1
-fi
-
 if [[ "$test_result" != "0" ]]; then
   echo "tests failed!"
-  exit 2
+  exit 1
 fi
