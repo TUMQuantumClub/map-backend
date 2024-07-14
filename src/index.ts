225e41e66307358be38200181a310af28079943d
biosfood <mail@lukas-eisenhauer.de>
2024-07-14 16:57:09 +0200

    Testing (#9)
    * add jest dependency
    
    * add first test
    
    * add prettier dependency
    
    * run add action to run prettier
    
    * create action to test
    
    * remove unneeded npm i from test.sh
    
    * move prettier check to the github action

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
-app.use(express.json({ limit: '50mb' }))
+app.use(
+  express.json({
+    limit: "50mb",
+  }),
+);
 
-app.listen(4000)
+app.listen(4000);
