7732fc60b6723b7328b5565916f920a360d7161d
biosfood <mail@lukas-eisenhauer.de>
2024-07-30 10:49:15 +0200

    Appollo server setup (#13)
    * fix tsconfig
    
    * bump dependencies
    
    * reformat auto-assign-action
    
    * fetch submodules when running tests
    
    * initialize apollo graphql server with schema
    
    * bump submodule

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
 import express from "express";
+import { ApolloServer } from "@apollo/server";
+import fs from "fs";
+import { expressMiddleware } from "@apollo/server/express4";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { config } from "dotenv";
+
+config();
 
 const app = express();
 app.use(
   express.json({
-    limit: "50mb",
+    limit: "5kb",
   }),
+);
+
+const schema = fs.readFileSync("map-docs/v1/schema.graphqls", "utf8");
+
+const apollo = new ApolloServer({
+  typeDefs: schema,
+});
+await apollo.start();
+
+app.use(
+  "/graphql",
+  cors<cors.CorsRequest>(),
+  bodyParser.json(),
+  expressMiddleware(apollo),
 );
 
 app.listen(4000);
--- tests/serverAvailable.spec.ts
+++ tests/serverAvailable.spec.ts
@@ -4,3 +4,9 @@ test("can reach server", async () => {
   const response = await fetch("http://localhost:4000");
   expect(response.status).toBe(404);
 });
+
+test("can reach apollo Grahpql endpoint", async () => {
+  const response = await fetch("http://localhost:4000/graphql");
+  // Just sending an empty query gives an error because we need a query to do something with graphql
+  expect(response.status).toBe(400);
+});
